6f6cea89dde9b9057fb5036a6728aeb1106cfd6b
5kWBassMachine <5kWBassMachine@gmail.com>
2020-10-26 23:04:48 +0100

    Initial Commit

---
+++ src/main/java/com/github/pr0lebenimholz/discordbot/Constants.java
@@ -0,0 +1,9 @@
+package com.github.pr0lebenimholz.discordbot;
+
+public class Constants {
+    public static final String PATH_CONFIG_FILE = "./discordbot.cfg";
+    public static final int VAL_DEFAULT_LOGLEVEL = 0;
+    public static final int VAL_DEFAULT_LOGLEVEL_FILE = 2;
+    public static final String PATH_LOG_DIR = "./logs/";
+    public static final int VAL_MAX_RESTARTS = 100;
+}
---
+++ src/main/java/com/github/pr0lebenimholz/discordbot/exceptions/TooMuchIterationException.java
@@ -0,0 +1,7 @@
+package com.github.pr0lebenimholz.discordbot.exceptions;
+
+public class TooMuchIterationException extends Exception {
+    public TooMuchIterationException(String s) {
+        super(s);
+    }
+}
